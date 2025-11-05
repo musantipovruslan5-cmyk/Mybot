8241341ead74123f9cd37bd2b67e518f4f61fae1
musantipovruslan5-cmyk <musantipovruslan5@gmail.com>
2025-11-05 21:26:00 +0400

    Первый коммит

--- config.py
+++ config.py
@@ -1,6 +1,26 @@
 import os
 
-# === Переменные среды (Render Environment Variables) ===
+# Локальная версия без переменных окружения
+TOKEN = "8562829264:AAHsrKNbET63BzX9Df8U7zxEZpd8SMb6bG0"  # Токен бота
+admin = 7489815425  # Chat ID администратора
+
+chat_bota = 'https://t.me/MalotNFT'
+instruction = 'Отсутствует'
+nicknameadm = 'malotNFT'
+procent = 6
+chat_id_bot = admin
+channel_id = admin
+db = 'db.db'
+
+replenish = (
+    '⚠️ Пополнение баланса\n\n'
+    '🥝 Qiwi \n\n'
+    f'👉 Номер(Qiwi) - <b><code>{admin}</code></b>\n'
+    '👉 Комментарий - <code>{code}</code>\n'
+    '👉 До 15 000 рублей!'
+)
+
+com_percent = 10# === Переменные среды (Render Environment Variables) ===
 TOKEN = os.environ.get("TOKEN")      # Токен бота
 admin = int(os.environ.get("ADMIN")) # Chat ID администратора
 
